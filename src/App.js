76472be24dac974d3131ea8319859dd297c8e93d
MD. ARIFUL HASAN TOTUL <mdahtotul@gmail.com>
2021-09-29 16:18:44 +0600

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from "./components/About/About";
+import "./App.css";
+import Home from "./components/Home/Home";
+import Friends from "./Friends/Friends";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <Router>
+         <Switch>
+            {/* default route  */}
+            <Route exact path="/">
+               <Home></Home>
+            </Route>
+
+            {/* home route  */}
+            <Route path="/home">
+               <Home></Home>
+            </Route>
+
+            {/* friends route  */}
+            <Route path="/friends">
+               <Friends></Friends>
+            </Route>
+
+            {/* about route  */}
+            <Route path="/about">
+               <About></About>
+            </Route>
+
+            {/* not found route  */}
+            <Route path="*">
+               <NotFound></NotFound>
+            </Route>
+
+            {/* switch end  */}
+         </Switch>
+      </Router>
+   );
 }
 
 export default App;
